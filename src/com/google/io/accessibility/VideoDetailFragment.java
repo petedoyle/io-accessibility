32103615ee07bef0d959c75494db4a676496753e
Pete Doyle <petedoyle@gmail.com>
2011-03-30 10:50:05 -0700

    Remove unused imports

--- src/com/google/io/accessibility/VideoDetailFragment.java
+++ src/com/google/io/accessibility/VideoDetailFragment.java
@@ -11,8 +11,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.io.accessibility.data.Video;
